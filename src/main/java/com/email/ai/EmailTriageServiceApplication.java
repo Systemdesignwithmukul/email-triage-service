621790cf381f8dc778204da2d9f3b9d4d6615dc1
MUKUL20BCS2002 <cmukul2019@gmail.com>
2025-09-12 19:34:31 +0530

    resolving dependencies

---
+++ src/main/java/com/email/ai/EmailTriageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.email.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailTriageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailTriageServiceApplication.class, args);
+	}
+
+}
